590875c15d89d0447210480aefdfdce7c6618d2f
Martin Risseeuw <hello@martinr.nl>
2015-08-14 11:41:52 +0200

    Changes style of homeflow

---
+++ public/fields/gr-amsterdam/settings.js
@@ -0,0 +1,9 @@
+fieldSettings['gr-amsterdam'] = {
+	particleColor: 'rgb(255,255,255)',
+	backgroundColor: 'rgb(0,132,168)',
+	trailLength: .3,
+	particleSpeed: 6,
+	particleDensity: 0.3,
+	particleMaxAge: 500,
+	timeStep: 18/18
+};
