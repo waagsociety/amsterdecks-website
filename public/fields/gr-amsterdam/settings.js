2b0b341eb59e22a7ce116723924714a4cc46640f
Martin Risseeuw <hello@martinr.nl>
2015-08-14 14:04:30 +0200

    Home flow settings changed

--- public/fields/gr-amsterdam/settings.js
+++ public/fields/gr-amsterdam/settings.js
@@ -1,9 +1,9 @@
 fieldSettings['gr-amsterdam'] = {
-	particleColor: 'rgb(255,255,255)',
-	backgroundColor: 'rgb(0,132,168)',
-	trailLength: .3,
+	particleColor: 'rgb(171,203,216)',
+	backgroundColor: 'rgb(0,76,115 )',
+	trailLength: 1,
 	particleSpeed: 3,
-	particleDensity: 0.25,
+	particleDensity: 0.45,
 	particleMaxAge: 500,
 	timeStep: 18/18
 };
