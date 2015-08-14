2af4c90c8bf30804ad01bd1605bb4eecb783114e
Martin Risseeuw <hello@martinr.nl>
2015-08-14 14:37:38 +0200

    Style update, added pages, added stroming placeholder

--- public/fields/gr-amsterdam/settings.js
+++ public/fields/gr-amsterdam/settings.js
@@ -1,6 +1,6 @@
 fieldSettings['gr-amsterdam'] = {
 	particleColor: 'rgb(171,203,216)',
-	backgroundColor: 'rgb(0,76,115 )',
+	backgroundColor: 'rgb(0,50,78)',
 	trailLength: 1,
 	particleSpeed: 3,
 	particleDensity: 0.45,
