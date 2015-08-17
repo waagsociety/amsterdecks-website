bbb42068eef32f2edd491f7524998fd65f52c6ae
Martin Risseeuw <hello@martinr.nl>
2015-08-17 17:20:30 +0200

    Style updates

--- public/fields/overstorten/settings.js
+++ public/fields/overstorten/settings.js
@@ -2,7 +2,7 @@ fieldSettings['overstorten'] = {
 	particleColor: 'rgb(150,150,150)',
 	backgroundColor: 'rgb(7,5,17)',
 	trailLength: 1,
-	particleSpeed: 100,
+	particleSpeed: 6,
 	particleDensity: 0.1,
 	particleMaxAge: 1200,
 	timeStep: 18/7,
